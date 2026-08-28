fa3de528dcc95a81afaca8ee91460ffc51e2c949
nitin027 <madhaninitin27@gmail.com>
2026-08-28 22:09:27 +0530

    parcel bundler

--- App.js
+++ App.js
@@ -5,9 +5,15 @@
 //if we want multiple child ele we can use an array also
 //root.render will completely replace eveything if there is anything already 
 //inside div ele with root id 
+
+import React from "react";
+//const React = require('react') this also work its sync in nature package is coming from node modules
+import ReactDOM from "react-dom/client"
  
- const heading = React.createElement("h1",{},[
-    React.createElement("span",{id:"ele1"},"span inside h1")
- ])
+ const heading = React.createElement("h1",{},
+    [React.createElement("span",{id:"ele1",key:"ele1"},"span inside h1"),
+     React.createElement("span",{id:"ele2",key:"ele2"},"span2 inside h1")   
+    ]
+ )
         const root = ReactDOM.createRoot(document.getElementById("root"))
         root.render(heading)
