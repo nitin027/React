3f5ef11e375c79909da9f4fb9875a3ec8e2ac3af
nitin027 <madhaninitin27@gmail.com>
2026-08-29 12:53:47 +0530

    jsx and react functional component

--- App.js
+++ App.js
@@ -10,10 +10,41 @@ import React from "react";
 //const React = require('react') this also work its sync in nature package is coming from node modules
 import ReactDOM from "react-dom/client"
  
- const heading = React.createElement("h1",{},
-    [React.createElement("span",{id:"ele1",key:"ele1"},"span inside h1"),
-     React.createElement("span",{id:"ele2",key:"ele2"},"span2 inside h1")   
-    ]
- )
+//  const heading = React.createElement("h1",{},
+//     [React.createElement("span",{id:"ele1",key:"ele1"},"span inside h1"),
+//      React.createElement("span",{id:"ele2",key:"ele2"},"span2 inside h1")   
+//     ]
+//  )
+
+//JSX (transpiled before is reaches the js) - PARCEL - Babel transplies it
+
+//JSX => React.createElement() => React Element - JS object => HTMLElement(render)
+
+//React Element
+const heading = (<h1 className="heading" tabIndex="1">
+    Heading from h1 and jsx
+</h1>)
+let name ="Nitin"
+console.log(heading) // js object
+
+//React Functional component
+const Component = ()=> {
+    return  ( 
+        //jsx start
+        <>  
+      <div id="container"> 
+        {name} //writing javascript inside jsx
+         <h2>
+            Heading h2 from functional component</h2>
+      </div> 
+      {name}
+</> 
+//jsx end
+    //  {name} // this is not valid as its not inside jsx and jsx should be wrapped inside one parent element
+        
+        
+     ) 
+    }
+
         const root = ReactDOM.createRoot(document.getElementById("root"))
-        root.render(heading)
+        root.render(<Component />)
